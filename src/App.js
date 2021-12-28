914936cf752008a10078933f3fc51f33246ac97c
Mayu Uchida <31529564+UCDM@users.noreply.github.com>
2021-12-28 13:04:54 +0900

    than catch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import axios from "axios";
 
 export default function App() {
   const onClickUsers = () => {
-    axios.get("");
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   const onClickUser1 = () => {
     alert("User1");
